d72c14f646f30aa2523a7e37c3b4f2db2748c5f0
Edouard Evangelisti <edouard.evangelisti@inrae.fr>
2025-08-10 09:49:09 +0200

    Replacing "check" by "preprocess"

--- hfinder.py
+++ hfinder.py
@@ -6,7 +6,7 @@ import hfinder_folders as HFinder_folders
 import hfinder_settings as HFinder_settings
 
 
-def generate_training_dataset(args):
+def preprocess(args):
     HFinder_settings.load(args)
     HFinder_settings.print_summary()
     HFinder_folders.create_session_folders()
@@ -14,21 +14,21 @@ def generate_training_dataset(args):
 
 
 def train(args):
-    generate_training_dataset(args)
+    preprocess(args)
     HFinder_train.run()
 
 
 def main():
-    parser = argparse.ArgumentParser(prog="hfinder", description="HFinder CLI for hyphae datasets")
+    parser = argparse.ArgumentParser(prog="hfinder", description="Computer vision for plant-microbe interfaces")
     subparsers = parser.add_subparsers(title="subcommands", dest="command")
     subparsers.required = True
 
-    # ---- Subcommand: check_masks ----
-    parser_check = subparsers.add_parser("check", help="Generate and validate binary masks")
-    HFinder_settings.define_arguments(parser_check, "check")
-    parser_check.set_defaults(func=generate_training_dataset)
+    # ---- Subcommand: preprocessing ----
+    parser_check = subparsers.add_parser("preprocess", help="Generate and validate binary masks")
+    HFinder_settings.define_arguments(parser_check, "preprocess")
+    parser_check.set_defaults(func=preprocess)
 
-    # ---- Subcommand: train ----
+    # ---- Subcommand: training ----
     parser_train = subparsers.add_parser("train", help="Train YOLOv8 model on hyphae dataset")
     HFinder_settings.define_arguments(parser_train, "train")
     parser_train.set_defaults(func=train)
