8c7377447c9f96610a6429f6479041f9206fba68
Edouard Evangelisti <edouard.evangelisti@inrae.fr>
2025-08-12 10:24:22 +0200

    Documentation

--- hfinder_imageinfo.py
+++ hfinder_imageinfo.py
@@ -52,38 +52,83 @@ def initialize():
 
 
 def set_current_image(img_name):
+    """
+    Set the currently active image and log the operation.
+
+    :param img_name: Name of the image to process.
+    :type img_name: str
+    :rtype: None
+    """
     global CURRENT_IMAGE
     CURRENT_IMAGE = img_name
     HFinder_log.info(f"Processing {img_name}")
 
 
+
 def get_current(cls):
+    """
+    Retrieve the instruction dictionary for a given class in the current image.
+
+    :param cls: Class name.
+    :type cls: str
+    :rtype: dict | int
+    """
     return CLASS_INSTRUCTIONS[CURRENT_IMAGE][cls]
 
 
 
 def image_has_instructions():
+    """
+    Check if the current image has associated instructions.
+
+    :rtype: bool
+    """
     return CURRENT_IMAGE in CLASS_INSTRUCTIONS
 
 
+
 def from_frame(cls, default=0):
+    """
+    Get the starting frame index for a class, zero-based.
+
+    :param cls: Class name.
+    :type cls: str
+    :param default: Value returned if no valid frame is found.
+    :type default: int
+    :rtype: int
+    """
     try:
         x = int(get_current(cls)["from"])
-        return default if x <= 0 else x 
+        return default if x <= 0 else x - 1
     except:
         return default
 
 
+
 def to_frame(cls, default=0):
+    """
+    Get the ending frame index for a class, zero-based.
+
+    :param cls: Class name.
+    :type cls: str
+    :param default: Value returned if no valid frame is found.
+    :type default: int
+    :rtype: int
+    """
     try:
         x = int(get_current(cls)["to"])
-        return default if x <= 0 else x 
+        return default if x <= 0 else x - 1
     except:
         return default
 
 
 
 def get_classes():
+    """
+    List the classes that have valid instructions for the current image.
+
+    :rtype: list[str]
+    """
     try:
         classes = CLASS_INSTRUCTIONS[CURRENT_IMAGE].keys()
         return [cls for cls in classes if get_current(cls) != -1]
@@ -93,8 +138,11 @@ def get_classes():
 
 def get_channel(cls):
     """
-    Return the index of the channel corresponding to class `cls` in the current 
-    image.
+    Get the channel index for a given class in the current image.
+
+    :param cls: Class name.
+    :type cls: str
+    :rtype: int | None
     """
     try:
         return get_current(cls)["channel"]
@@ -104,7 +152,11 @@ def get_channel(cls):
 
 def get_threshold(cls):
     """
-    Return the threshold for class `cls` in the current image.
+    Get the threshold value for a given class in the current image.
+
+    :param cls: Class name.
+    :type cls: str
+    :rtype: float | int | None
     """
     try:
         return get_current(cls)["threshold"]
@@ -114,8 +166,11 @@ def get_threshold(cls):
 
 def get_manual_segmentation(cls):
     """
-    Return the name of the JSON file containing manual segmentation for class 
-    `cls` in the current image.
+    Get the filename of the manual segmentation for a given class.
+
+    :param cls: Class name.
+    :type cls: str
+    :rtype: str | None
     """
     try:
         return get_current(cls)["segment"]
@@ -124,13 +179,17 @@ def get_manual_segmentation(cls):
 
 
 def allows_MIP_generation(cls):
+    """
+    Check whether MIP (Maximum Intensity Projection) generation is allowed
+    for a given class in the current image.
+
+    :param cls: Class name.
+    :type cls: str
+    :rtype: bool
+    """
     try:
         return get_current(cls)["MIP"] > 0
     except:
         return True
-
-
-
-
 
 
