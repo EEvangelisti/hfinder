0b2fefd113a86b8b84a3ac44f5367246cd8a5a64
Edouard Evangelisti <edouard.evangelisti@inrae.fr>
2025-09-10 22:02:21 +0200

    Populating CLI

---
+++ src/hfinder/session/__init__.py
@@ -0,0 +1,46 @@
+
+from .folders import (
+    create_session_folders,
+    get_root,
+    rootify,
+    get_log_dir,
+    get_runs_dir,
+    get_dataset_dir,
+    get_image_train_dir,
+    get_label_train_dir,
+    get_image_val_dir,
+    get_label_val_dir,
+    get_masks_dir,
+    get_contours_dir
+)
+
+from .settings import (
+    compatible_modes,
+    define_arguments,
+    load,
+    get,
+    set,
+    print_summary
+)
+
+__all__ = [
+    "create_session_folders",
+    "get_root",
+    "rootify",
+    "get_log_dir", 
+    "get_runs_dir",
+    "get_dataset_dir",
+    "get_image_train_dir", 
+    "get_label_train_dir",
+    "get_image_val_dir",
+    "get_label_val_dir", 
+    "get_masks_dir",
+    "get_contours_dir",
+    "compatible_modes",
+    "define_arguments",
+    "load",
+    "get",
+    "set",
+    "print_summary"
+]
+
