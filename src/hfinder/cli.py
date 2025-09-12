7d62248a27211a336c708444b95010a05d2591b5
Edouard Evangelisti <edouard.evangelisti@inrae.fr>
2025-09-12 23:07:25 +0200

    Renaming toolbox/ (folder) as extra/

--- src/hfinder/cli.py
+++ src/hfinder/cli.py
@@ -5,15 +5,15 @@ def main() -> int:
     return _main()
 
 def annot2images() -> int:
-    from hfinder.toolbox.annot2images import main as _main
+    from hfinder.extra.annot2images import main as _main
     return _main()
 
 def annot2signal() -> int:
-    from hfinder.toolbox.annot2signal import main as _main
+    from hfinder.extra.annot2signal import main as _main
     return _main()
 
 def annot2distances() -> int:
-    from hfinder.toolbox.annot2distances import main as _main
+    from hfinder.extra.annot2distances import main as _main
     return _main()
 
 if __name__ == "__main__":
